c8cc75bf6c0b9b38d0f4cab55360c746975e8f26
Akasha Sahu <akashasahu2001@gmail.com>
2025-05-13 20:40:44 +0530

    app component update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Currency-Converter';
+
+  currency: any;
+
+  // fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json')
+  // .then()
 }
