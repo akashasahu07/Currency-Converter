bb501b6821e19808ef7a515762ac93e46c90ad05
Akasha Sahu <akashasahu2001@gmail.com>
2025-05-15 23:30:25 +0530

    api data fetch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Currency-Converter';
 
-  currency: any;
+  currencies:any = [];
+  fromCurrency: any;
+  toCurrency: any;
+  fromVal: number = 0;
+  toVal: number = 0;
 
-  // fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json')
-  // .then()
+  constructor() {
+    fetch('https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json')
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data)
+        this.currencies = Object.values(data);
+        // console.log(this.currencies)
+      })
+  }
+
+  onFromCurrency(event: any) {
+    this.fromCurrency=event.target.value
+    console.log(event.target.value)
+  }
+
+  onToCurrency(event: any) {
+    console.log(event.target.value)
+  }
+
+  fromValue(event: any) {
+    this.fromVal = event.target.value
+    console.log(this.fromVal)
+  }
+
+  Calculate() {
+    fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${this.fromCurrency}.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+      })
+  }
 }
